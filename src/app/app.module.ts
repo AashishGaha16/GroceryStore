e9f062ca87a091c380847d4df96b0b88d67b1682
Aashishgaha <aashishgahamagar@icloud.com>
2022-03-29 17:03:56 +0545

    updates to deploy to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxScrollTopModule } from 'ngx-scrolltop';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { NgxScrollTopModule } from 'ngx-scrolltop';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxScrollTopModule
+    NgxScrollTopModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
