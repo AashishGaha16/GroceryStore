ceeb6cdd825e939f62ec2936ea78474b4f5041b7
Aashishgaha <aashishgahamagar@icloud.com>
2022-03-30 22:46:17 +0545

    Converting static banner to dynamic banner and payment responsive tab UI solved.

--- src/app/main/banner/banner/banner.component.ts
+++ src/app/main/banner/banner/banner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, Event as NavigationEvent, NavigationEnd } from '@angular/router';
+
 
 @Component({
   selector: 'app-banner',
@@ -8,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BannerComponent implements OnInit {
 
-  constructor() { }
+  banners: string[] = []
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.router.events.subscribe((event:NavigationEvent)=>{
+      if(event instanceof NavigationEnd) {
+        this.banners = this.router.url.split("/").slice(1);
+      }
+    })
   }
 
 }
